019fe791cae3ad4d8d5affc50cfcb08cb6fb79c2
Estevão Amaro Baptista de Oliveira <PET0159769@aluno.firjansenaisesi.com.br>
2025-06-23 13:50:18 -0300

    atualização de css

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
   }
 
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=_" />
 
